4e66214aa4c5fadb72f44e8cf9b64f45a8878b96
jia680806 <32798328+jia680806@users.noreply.github.com>
2022-08-23 01:16:23 +0800

    test OssTemplate

---
+++ tanhua-app-server/src/test/java/com/jia/test/OssTemplateTest.java
@@ -0,0 +1,28 @@
+package com.jia.test;
+
+import com.jia.autoconfig.template.OssTemplate;
+import com.jia.server.AppServerApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes =AppServerApplication.class)
+public class OssTemplateTest {
+
+    @Autowired
+    private OssTemplate ossTemplate;
+    @Test
+    public void testOssTemplate() throws FileNotFoundException {
+        String path = "H:\\img\\0.jpg";
+        FileInputStream inputStream = new FileInputStream(new File(path));
+        String url = ossTemplate.upload(path, inputStream);
+        System.out.println(url);
+    }
+}
