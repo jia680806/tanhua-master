b26c81464c50e4c9c78823a23432d36173b55d5b
jia680806 <32798328+jia680806@users.noreply.github.com>
2022-08-23 13:56:32 +0800

    add AipFaceTemplate

---
+++ tanhua-autoconfig/src/main/java/com/jia/autoconfig/template/AipFaceTemplate.java
@@ -0,0 +1,31 @@
+package com.jia.autoconfig.template;
+
+
+import com.baidu.aip.face.AipFace;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class AipFaceTemplate {
+
+    private AipFace client;
+
+    public Boolean detect(String imageUrl){
+        String imageType = "URL";
+
+        HashMap<String, String> options = new HashMap<String, String>();
+        options.put("face_field", "age");
+        options.put("max_face_num", "2");
+        options.put("face_type", "LIVE");
+        options.put("liveness_control", "LOW");
+
+
+        // 人脸检测
+        JSONObject res = client.detect(imageUrl, imageType, options);
+        System.out.println(res.toString(2));
+         Integer errorCode  = (Integer) res.get("error_code");
+
+         return errorCode == 0;
+
+    }
+}
