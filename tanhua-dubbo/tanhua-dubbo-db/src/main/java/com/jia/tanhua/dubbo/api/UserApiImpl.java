af9e287dae718ad8378afbd5de8ffb012f115e95
jia680806 <32798328+jia680806@users.noreply.github.com>
2022-08-23 17:05:32 +0800

    update package

--- tanhua-dubbo/tanhua-dubbo-db/src/main/java/com/jia/tanhua/dubbo/api/UserApiImpl.java
+++ tanhua-dubbo/tanhua-dubbo-db/src/main/java/com/jia/tanhua/dubbo/api/UserApiImpl.java
@@ -1,9 +1,8 @@
-package com.jia.tanhua.dubbo.apiimpl;
+package com.jia.tanhua.dubbo.api;
 
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.jia.tanhua.domain.User;
-import com.jia.tanhua.dubbo.api.UserApi;
 import com.jia.tanhua.dubbo.mappers.UserMapper;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
