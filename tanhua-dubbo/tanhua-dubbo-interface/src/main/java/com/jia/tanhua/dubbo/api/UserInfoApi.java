2daa11e187a7afb92b4c1eb22814b9e8dab278d1
jia680806 <32798328+jia680806@users.noreply.github.com>
2022-08-23 18:19:29 +0800

    update UserInfoApi

---
+++ tanhua-dubbo/tanhua-dubbo-interface/src/main/java/com/jia/tanhua/dubbo/api/UserInfoApi.java
@@ -0,0 +1,8 @@
+package com.jia.tanhua.dubbo.api;
+
+import com.jia.tanhua.domain.UserInfo;
+
+public interface UserInfoApi {
+    public void save(UserInfo userInfo);
+    public void update(UserInfo userInfo);
+}
