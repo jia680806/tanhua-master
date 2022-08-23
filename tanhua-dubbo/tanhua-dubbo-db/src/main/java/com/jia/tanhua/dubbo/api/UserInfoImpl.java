a634333829dd9b662df60f1949ca56a4aafdc3f5
jia680806 <32798328+jia680806@users.noreply.github.com>
2022-08-23 19:11:32 +0800

    add UserInfomapper

---
+++ tanhua-dubbo/tanhua-dubbo-db/src/main/java/com/jia/tanhua/dubbo/api/UserInfoImpl.java
@@ -0,0 +1,24 @@
+package com.jia.tanhua.dubbo.api;
+
+import com.jia.tanhua.domain.UserInfo;
+import com.jia.tanhua.dubbo.mappers.UserInfoMapper;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@DubboService
+public class UserInfoImpl implements UserInfoApi {
+
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+    @Override
+    public void save(UserInfo userInfo) {
+        userInfoMapper.insert(userInfo);
+
+    }
+
+    @Override
+    public void update(UserInfo userInfo) {
+        userInfoMapper.updateById(userInfo);
+
+    }
+}
---
+++ tanhua-dubbo/tanhua-dubbo-db/src/main/java/com/jia/tanhua/dubbo/mappers/UserInfoMapper.java
@@ -0,0 +1,8 @@
+package com.jia.tanhua.dubbo.mappers;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jia.tanhua.domain.UserInfo;
+
+public interface UserInfoMapper extends BaseMapper<UserInfo> {
+
+}
