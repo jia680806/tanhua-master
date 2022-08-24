8ce9120599a585a5dda8ca1717b732e653285de0
jia680806 <32798328+jia680806@users.noreply.github.com>
2022-08-24 16:21:30 +0800

    add ExceptionAdvice

---
+++ tanhua-app-server/src/main/java/com/jia/tanhua/server/exception/ExceptionAdvice.java
@@ -0,0 +1,20 @@
+package com.jia.tanhua.server.exception;
+
+import com.jia.tanhua.vo.ErrorResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionAdvice {
+    @ExceptionHandler(BusinessException.class)
+    public ResponseEntity MyExceptionHander(BusinessException be){
+        ErrorResult errorResult = be.getErrorResult();
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResult);
+    }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity MyAllExceptionHander(Exception exception){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ErrorResult.error());
+    }
+}
